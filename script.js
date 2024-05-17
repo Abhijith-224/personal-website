1308d198cb9086f54852c3fbf6c19bf54d6ec65e
MuhdAdil13 <adilrahim333@gmail.com>
2024-05-17 11:15:30 +0530

    hehehe

---
+++ script.js
@@ -0,0 +1,23 @@
+//For Smooth Scrolling
+
+const navLinks = document.querySelectorAll('.mylink');
+const nav = document.querySelector('nav');
+navLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default anchor link behavior
+    const targetSection = document.getElementById(this.hash.substring(1));
+
+    // Enhanced smooth scrolling with optional offset for navbar height
+    if (window.innerWidth <= 768) { // Adjust the width as needed for your mobile breakpoint
+        window.scrollTo({
+          top: targetSection.offsetTop - 80,
+          behavior: 'smooth'
+        });
+      } else {
+        window.scrollTo({
+          top: targetSection.offsetTop - (nav.offsetHeight || 50),
+          behavior: 'smooth'
+        });
+      }
+  });
+});
