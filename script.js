d8d72c323b0593a22bbd252176cba53d4b323389
Abhijith-224 <abhijithvichuhpd345@gmail.com>
2024-06-12 12:42:30 +0530

    contact js

--- script.js
+++ script.js
@@ -3,26 +3,26 @@
 const navLinks = document.querySelectorAll('.custom-nav-item');
 const nav = document.querySelector('nav');
 navLinks.forEach(link => {
-  link.addEventListener('click', function(event) {
+  link.addEventListener('click', function (event) {
     event.preventDefault(); // Prevent default anchor link behavior
     const targetSection = document.getElementById(this.hash.substring(1));
 
     // Enhanced smooth scrolling with optional offset for navbar height
     if (window.innerWidth <= 768) { // Adjust the width as needed for your mobile breakpoint
-        window.scrollTo({
-          top: targetSection.offsetTop - 80,
-          behavior: 'smooth'
-        });
-      } else {
-        window.scrollTo({
-          top: targetSection.offsetTop - (nav.offsetHeight + 20),
-          behavior: 'smooth'
-        });
-      }
+      window.scrollTo({
+        top: targetSection.offsetTop - 80,
+        behavior: 'smooth'
+      });
+    } else {
+      window.scrollTo({
+        top: targetSection.offsetTop - (nav.offsetHeight + 20),
+        behavior: 'smooth'
+      });
+    }
   });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Get the height of the navbar element
   var navbarHeight = document.querySelector('.navbar').offsetHeight;
 
@@ -49,7 +49,7 @@ AOS.init({
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
+
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
@@ -65,16 +65,60 @@ AOS.init({
 
 // For Navbar fading
 
-$(document).ready(function(){
-  $('.navbar-nav a').on('click', function(){
+$(document).ready(function () {
+  $('.navbar-nav a').on('click', function () {
 
-    if($('.navbar-collapse').hasClass('show'))
-    { 
+    if ($('.navbar-collapse').hasClass('show')) {
       $('.navbar-collapse').removeClass('show');
       $('#MyCheckbox').prop('checked', false);
-    }
-  });
+    }
+  });
 });
+
+
+//For Contact Email
+
+const form = document.querySelector('#contactfrm');
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+
+  const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}
+  <br> Subject: ${subject.value}<br> Message: ${message.value}`;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "abhijiths2907@gmail.com",
+    Password: "CDAD1D37D53E7E4FC1F1A71C85C37D65D011",
+    To: 'abhijiths2907@gmail.com',
+    From: "abhijiths2907@gmail.com",
+    Subject: "hgfhtfghd",
+    Body: bodyMessage
+  }).then(
+   
+    message => {
+      if(message == "OK"){
+        Swal.fire({
+          title: "Success!",
+          text: "Message Send Successfully!",
+          icon: "success"
+        });
+      }
+    }
+  );
+}
+
+// form.addEventListener("submit", (e) => {
+//   alert("ok")
+//   e.preventDefault();
+
+//   sendEmail();
+
+// });
+
 
 
 
